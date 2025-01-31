8ba7277d4a88b8f08de9cc3010ce3b2204ac2497
sandy112-gh <sandyngo112@gmail.com>
2025-01-30 21:24:29 -0500

    fix countries data

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,11 @@
 const Navigation = () => {
-  return <div></div>
+  return
+  ;<nav>
+    <div>Home</div>
+    <div>Blog</div>
+    <div>Travel Tips</div>
+    <div>Contact</div>
+  </nav>
 }
 
 export default Navigation
