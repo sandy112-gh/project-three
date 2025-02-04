c75d3047e2d12e250c125fd0943c12e9f6c1a12d
sandy112-gh <sandyngo112@gmail.com>
2025-02-04 18:14:45 -0500

    clean up import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import './App.css'
-import countries from './countries.json'
 import Navigation from './components/Navigation'
 import HeaderBanner from './components/HeaderBanner'
-import Country from './components/Country'
 import ArticleSection from './components/ArticleSection'
-import ArticleCard from './components/ArticleCard'
 import HighlightSection from './components/HighlightSection'
 import TopCountrySection from './components/TopCountrySection'
 
