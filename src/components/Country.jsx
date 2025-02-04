15ee4f60516ae281466616d3857dc14a718138b7
sandy112-gh <sandyngo112@gmail.com>
2025-02-04 16:44:47 -0500

    set highlight column layout

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,8 +1,9 @@
-const Country = (props) => {
+const Country = ({ Thailand }) => {
   return (
     <div>
-      <h5>Country: {props.countryName}</h5>
-      <h5>Capital: {props.capital}</h5>
+      <h5>Country: {Thailand.countryName}</h5>
+      <h5>Capital: {Thailand.capital}</h5>
+      <h5>Currency: {Thailand.currency}</h5>
     </div>
   )
 }
--- src/components/HighlightSection.jsx
+++ src/components/HighlightSection.jsx
@@ -2,18 +2,23 @@ import Country from './Country'
 import countries from '../countries.json'
 
 const HighlightSection = () => {
+  const Thailand = countries.countries[0]
   return (
     <div class="highlightSectionStyle">
       <h2 class="highlightSectionTitle">Spotlight on Thailand:</h2>
       <div>
+        <Country Thailand={Thailand} />
+      </div>
+      {/* <div>
         {countries.countries.map((country) => (
           <Country
             key={country.countryName}
             countryName={country.countryName}
             capital={country.capital}
+            currency={country.currency}
           />
         ))}
-      </div>
+      </div> */}
     </div>
   )
 }
